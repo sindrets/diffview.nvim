9b5c1760c632f0dd86b08edae8fd550a0b2e756d
Sindre T. Strøm <sindrets@gmail.com>
2021-08-18 20:22:50 +0200

    Disable stylua in config.

--- lua/diffview/config.lua
+++ lua/diffview/config.lua
@@ -5,6 +5,7 @@ function M.diffview_callback(cb_name)
   return string.format("<Cmd>lua require'diffview'.trigger_event('%s')<CR>", cb_name)
 end
 
+-- stylua: ignore start
 M.defaults = {
   diff_binaries = false,
   file_panel = {
@@ -16,31 +17,32 @@ M.defaults = {
   key_bindings = {
     disable_defaults = false,
     view = {
-      ["<tab>"] = M.diffview_callback("select_next_entry"),
-      ["<s-tab>"] = M.diffview_callback("select_prev_entry"),
+      ["<tab>"]     = M.diffview_callback("select_next_entry"),
+      ["<s-tab>"]   = M.diffview_callback("select_prev_entry"),
       ["<leader>e"] = M.diffview_callback("focus_files"),
       ["<leader>b"] = M.diffview_callback("toggle_files"),
     },
     file_panel = {
-      ["j"] = M.diffview_callback("next_entry"),
-      ["<down>"] = M.diffview_callback("next_entry"),
-      ["k"] = M.diffview_callback("prev_entry"),
-      ["<up>"] = M.diffview_callback("prev_entry"),
-      ["<cr>"] = M.diffview_callback("select_entry"),
-      ["o"] = M.diffview_callback("select_entry"),
+      ["j"]             = M.diffview_callback("next_entry"),
+      ["<down>"]        = M.diffview_callback("next_entry"),
+      ["k"]             = M.diffview_callback("prev_entry"),
+      ["<up>"]          = M.diffview_callback("prev_entry"),
+      ["<cr>"]          = M.diffview_callback("select_entry"),
+      ["o"]             = M.diffview_callback("select_entry"),
       ["<2-LeftMouse>"] = M.diffview_callback("select_entry"),
-      ["-"] = M.diffview_callback("toggle_stage_entry"),
-      ["S"] = M.diffview_callback("stage_all"),
-      ["U"] = M.diffview_callback("unstage_all"),
-      ["X"] = M.diffview_callback("restore_entry"),
-      ["R"] = M.diffview_callback("refresh_files"),
-      ["<tab>"] = M.diffview_callback("select_next_entry"),
-      ["<s-tab>"] = M.diffview_callback("select_prev_entry"),
-      ["<leader>e"] = M.diffview_callback("focus_files"),
-      ["<leader>b"] = M.diffview_callback("toggle_files"),
+      ["-"]             = M.diffview_callback("toggle_stage_entry"),
+      ["S"]             = M.diffview_callback("stage_all"),
+      ["U"]             = M.diffview_callback("unstage_all"),
+      ["X"]             = M.diffview_callback("restore_entry"),
+      ["R"]             = M.diffview_callback("refresh_files"),
+      ["<tab>"]         = M.diffview_callback("select_next_entry"),
+      ["<s-tab>"]       = M.diffview_callback("select_prev_entry"),
+      ["<leader>e"]     = M.diffview_callback("focus_files"),
+      ["<leader>b"]     = M.diffview_callback("toggle_files"),
     },
   },
 }
+-- stylua: ignore end
 
 M._config = M.defaults
 
