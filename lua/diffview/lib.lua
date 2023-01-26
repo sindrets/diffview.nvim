b3a8a4fa7d8f621eadd299bda61be417411c5a2a
Sindre T. Strøm <sindrets@gmail.com>
2023-01-26 16:36:54 +0100

    fix: Check whether view has an open entry

--- lua/diffview/lib.lua
+++ lua/diffview/lib.lua
@@ -200,7 +200,7 @@ function M.is_buf_in_use(bufnr)
   for _, view in ipairs(M.views) do
     if view:instanceof(StandardView.__get()) then
       ---@cast view StandardView
-      for _, file in ipairs(view.cur_entry.layout:files()) do
+      for _, file in ipairs(view.cur_entry and view.cur_entry.layout:files() or {}) do
         if file:is_valid() and file.bufnr == bufnr then
           return true
         end
