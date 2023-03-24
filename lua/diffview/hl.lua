6420a73b340fdb1f842479cd7640dcca9ec6f5d1
3699394 <uploadvideo1001@gmail.com>
2023-03-24 10:39:17 +0800

    use `nvim_get_hl` if available

--- lua/diffview/hl.lua
+++ lua/diffview/hl.lua
@@ -121,12 +121,12 @@ function M.get_hl(name, no_trans)
   local hl
 
   if no_trans then
-    hl = api.nvim__get_hl_defs(0)[name]
+    hl = api.nvim__get_hl_defs and api.nvim__get_hl_defs(0)[name] or api.nvim_get_hl(0, {name=name})
   else
     local id = api.nvim_get_hl_id_by_name(name)
 
     if id then
-      hl = api.nvim_get_hl_by_id(id, true)
+      hl = api.nvim_get_hl_by_id and api.nvim_get_hl_by_id(id, true) or api.nvim_get_hl(0, {id=id})
     end
   end
 
