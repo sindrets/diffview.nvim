1fc42e67dfb2da7946edbc9a30cb98ed371b8dde
Sindre T. Strøm <sindrets@gmail.com>
2023-05-15 11:48:36 +0200

    fix(ffi): 'expr_map_lock' was added in 0.9.0
    (rel. #357)

--- lua/diffview/ffi.lua
+++ lua/diffview/ffi.lua
@@ -4,6 +4,8 @@ local C = ffi.C
 
 local M = setmetatable({}, { __index = ffi })
 
+local HAS_NVIM_0_9 = vim.fn.has("nvim-0.9") == 1
+
 ---Check if the |textlock| is active.
 ---@return boolean
 function M.nvim_is_textlocked()
@@ -15,7 +17,12 @@ end
 ---@return boolean
 function M.nvim_is_locked()
   if vim.in_fast_event() then return true end
-  return C.textlock > 0 or C.allbuf_lock > 0 or C.expr_map_lock > 0
+
+  if HAS_NVIM_0_9 then
+    return C.textlock > 0 or C.allbuf_lock > 0 or C.expr_map_lock > 0
+  end
+
+  return C.textlock > 0 or C.allbuf_lock > 0 or C.ex_normal_lock > 0
 end
 
 ffi.cdef([[
@@ -26,9 +33,18 @@ ffi.cdef([[
   /// Non-zero when no buffer name can be changed, no buffer can be deleted and
   /// current directory can't be changed. Used for SwapExists et al.
   extern int allbuf_lock;
-
-  /// Running expr mapping, prevent use of ex_normal() and text changes
-  extern int expr_map_lock;
 ]])
+
+if HAS_NVIM_0_9 then
+  ffi.cdef([[
+    /// Running expr mapping, prevent use of ex_normal() and text changes
+    extern int expr_map_lock;
+  ]])
+else
+  ffi.cdef([[
+    /// prevent use of ex_normal()
+    extern int ex_normal_lock;
+  ]])
+end
 
 return M
